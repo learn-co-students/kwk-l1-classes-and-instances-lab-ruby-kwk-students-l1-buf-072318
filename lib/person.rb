3147c4d74dbe713fb23bc3c5f5f0b593f540c983
Abigail Kane <aekane403@gmail.com>
2018-07-27 19:39:52 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
 # person.rb
+
+class Person 
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+adele_goldberg = Person.new("adele_goldberg")
+alan_kay = Person.new("alan_kay")
+
